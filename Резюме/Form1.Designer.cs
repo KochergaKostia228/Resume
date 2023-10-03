08a83b58c52a0b737ea1f1a9b8e2cf6de7ec550a
kochergakoctia12345@gmail.com <kochergakoctia12345@gmail.com>
2023-10-03 17:49:46 +0300

    Home ex-2 was Done!

--- Резюме/Form1.Designer.cs
+++ Резюме/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bt_StartGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_StartGame
+            // 
+            this.bt_StartGame.Location = new System.Drawing.Point(307, 178);
+            this.bt_StartGame.Name = "bt_StartGame";
+            this.bt_StartGame.Size = new System.Drawing.Size(185, 84);
+            this.bt_StartGame.TabIndex = 0;
+            this.bt_StartGame.Text = "Почати гру";
+            this.bt_StartGame.UseVisualStyleBackColor = true;
+            this.bt_StartGame.Click += new System.EventHandler(this.bt_StartGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_StartGame);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button bt_StartGame;
     }
 }
--- Резюме/Form1.cs
+++ Резюме/Form1.cs
@@ -6,5 +6,38 @@ namespace Резюме
         {
             InitializeComponent();
         }
+
+        private void bt_StartGame_Click(object sender, EventArgs e)
+        {
+            int begin = 0;
+            int end = 10;
+            int result = 5;
+            while (begin != result && end != result)
+            {
+                string text = "Ваше число більше " + result + "?Якщо це саме те шукане число натисныть (Cancel)";
+                var resultDialog = MessageBox.Show(text, "Гра почалась", MessageBoxButtons.YesNoCancel);
+
+                if (resultDialog == DialogResult.Yes)
+                {
+                    begin = result;
+                    result += begin / 2;
+                }
+                else if (resultDialog == DialogResult.No)
+                {
+                    end = result - 1;
+                    result = end / 2;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var resultDialog1 = MessageBox.Show("Ваше задане число --> " + result + ".Хочете зіграти ще раз?", "Результат", MessageBoxButtons.YesNo);
+            if (resultDialog1 == DialogResult.Yes)
+            {
+                bt_StartGame_Click(sender, e);
+            }
+        }
     }
 }
